41ce15a13f22b2fb69bef9b8e63490720804bdeb
Bttushig <71488931+Bttushig@users.noreply.github.com>
2021-11-15 16:19:10 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleApp1.APIs;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AwsAccessKeyId = "AKI..............."; // <--Your acces key id here 
+            string AwsSecretAccessKey = "UEL....................................";   // <--Your Acces key here
+            string AwsZone = "ap-southeast-1"; // <--Your region
+
+            var service = new MyS3Service(AwsAccessKeyId, AwsSecretAccessKey, AwsZone);
+
+            service.CreateNewBucketAsync("tushigbucket1129", AwsZone);
+
+            service.UploadObjectAsync("tushigbucket1129", DateTime.Now.Ticks.ToString());
+
+            var uploadFileKey = DateTime.Now.Ticks.ToString(); // save the key created
+            service.UploadFileAsync("tushigbucket1129", uploadFileKey);
+
+            service.ReadObjectDataAsync("tushigbucket1129", uploadFileKey + "--1"); // Read the last uploaded image
+
+            service.DeleteObject("tushigbucket1129", uploadFileKey + "--1"); // Delete the last uploaded image
+
+            service.GetObjectsList("tushigbucket1129");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+��# AWSSDK.S3
+Энэ App нь Amazon S3 Cloud storage тай ажиллахад хэрэглэгдэнэ.
+
+��# AWSSDK.S3 Энэ App нь Amazon S3 Cloud storage тай ажиллахад хэрэглэгдэнэ.
+
+1.AWS Хаяг үүсгэсэн байх (IAM болон root гэсэн 2 хаяг үүснэ) 
+2.IAM User -ээр нэвтэрж өөрийн гэсэн Access keys - үүсгэнэ. 
+3.Үүний дараа уг файлийг татаж аван өөрийн компьютер дээр задлана. 
+3.ConsoleApp1>ConsoleApp1.csproj Гэсэн файлийг visual studio ашиглан нээнэ. 
+4.Нээсний дараа Program.cs гэсэн файл дээр даран орж дээр байрлах хэсэгт өөрийн Access ID болон Acces Key Оруулна. 
+5.Хэрэв амжилттай холбогдсон бол Create New Bucket, File Upload, Delete зэрэг үйлдэлийн програмуудыг APIs гэх фолдер дотор оруулж өгсөн байгаа. 
+
